3b76b5cfaa43401a0b40c43202cbc290462825c0
Jens L. Hammer <jens@lindberghammer.dk>
2026-01-28 12:46:14 +0100

    Renamed base.css to root.css

--- assets/js/js-components/api-fetch.js
+++ assets/js/js-components/api-fetch.js
@@ -4,5 +4,5 @@ fetch("https://swapi.info/api/films")
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
-    body.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+    // body.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
   });
